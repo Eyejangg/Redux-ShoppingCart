95b3ea2bd1b6c995f56021faef97a41f7099a267
Eyejangg <ptwptw1550@gmail.com>
2026-02-05 16:50:01 +0700

    Navbar-RealTime-

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 const Navbar = () => {
+
   const dispatch = useDispatch(); // เรียกใช้ useDispatch hook
+
+  const carts = useSelector((state) => state.carts || []);
+  const totalItems = carts.reduce((total, item) => total + item.quantity, 0);
+
+
   const handlePageChange = (pageType) => {
     dispatch({ type: pageType });
   };
@@ -18,8 +24,12 @@ const Navbar = () => {
             <button onClick={() => handlePageChange("MYCART")} className="btn btn-ghost btn-sm">My Cart</button>
           </div>
           <div tabIndex={0} className="btn btn-ghost btn-circle">
-            <button className="btn btn-ghost btn-circle"></button>
-
+            <div className="indicator">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+              </svg>
+              <span className="badge badge-sm indicator-item">{totalItems}</span>
+            </div>
           </div>
         </div>
         <div className="dropdown dropdown-end">
